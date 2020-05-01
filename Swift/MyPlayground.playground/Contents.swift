3cc42f218cdf332951f41ba602204a2ebd5c07be
Caleb Wells <calebrwells@gmail.com>
2020-04-30 21:17:53 -0600

    Day 001 part 0.

---
+++ Swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+// Simple Types
+
+// Variables
+var name = "Tim"
+var age = 59
+
+// Multi-line strings
+var appleProducts = """
+iPhone
+iPad
+Apple Watch
+MacBook Pro
+Mac Pro
+iMac
+"""
+
+// Doubles and booleans
+var pi = 3.141
+var isThisABoolean = true
+
+// String interpolation
+var timsAge = "Tim is \(age)"
+
+// Constants
+let isSwiftAwesome = true
+
+// Type annotations
+let year: Int = 2020
