7112ba96aca522707d63f1f382cd8c5b4bbfd98c
Caleb Wells <calebrwells@gmail.com>
2020-05-01 09:25:58 -0600

    Added a watchOS project that I'm starting over with I need to fix the package dependencies.

---
+++ Apps/watchOS/Countdown Timer/Countdown Timer WatchKit Extension/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Starting over with this project, I need to fix the package dependencies.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Apps/watchOS/Countdown Timer/Countdown Timer WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Countdown Timer WatchKit Extension
+//
+//  Created by Caleb Wells on 5/1/20.
+//  Copyright © 2020 Caleb Wells. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
