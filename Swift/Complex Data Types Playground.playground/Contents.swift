0e75621c320513c5ae65538a3556a3cf2d5b8e7d
Caleb Wells <calebrwells@gmail.com>
2020-05-02 14:15:27 -0600

    Playground update.

--- Swift/Complex Data Types Playground.playground/Contents.swift
+++ Swift/Complex Data Types Playground.playground/Contents.swift
@@ -1,3 +1,26 @@
 // Arrays
 let operatingSystems = ["iOS", "iPadOS", "macOS", "watchOS", "tvOS"]
 operatingSystems[2] // macOS
+
+// Sets
+let names = Set(["Steve", "Tim"])
+print(names) // prints ["Tim", "Steve"]
+
+// Tuples
+let ceo = (name: "Tim Cook", age: 59)
+ceo.name
+
+// Dictionaries
+let iPhones = [
+    "iPhone 11 and 11 Pro": 2019,
+    "iPhone XR": 2018
+]
+iPhones["iPhone XR"]
+
+// Enums
+enum Result {
+    case success
+    case failure
+}
+
+let result = Result.success
